219dd76b291fd73aba32bf9b1d60cec78932d067
Andrii Chernenko <andrii.chernenko@softwerk.se>
2018-04-20 11:50:53 +0200

    Refactoring, return hostname and port on server start

--- android/src/main/java/com/strainy/RNHttpServer/RNHttpServerModule.java
+++ android/src/main/java/com/strainy/RNHttpServer/RNHttpServerModule.java
@@ -1,13 +1,12 @@
 package com.strainy.RNHttpServer;
 
-import com.strainy.RNHttpServer.Server;
-
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReadableMap;
@@ -18,138 +17,96 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 import java.util.Map;
 import java.util.HashMap;
 
-import android.support.annotation.Nullable;
 import android.util.Log;
 
-public class RNHttpServerModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
+public class RNHttpServerModule extends ReactContextBaseJavaModule {
 
-  ReactApplicationContext reactContext;
+    ReactApplicationContext reactContext;
 
-  private static final String TAG = "RNHttpServer";
+    private static final String TAG = "RNHttpServer";
 
-  private static final String DEFAULT_PORT_KEY = "DEFAULT_PORT";
-  private static final String DEFAULT_TIMEOUT_KEY = "DEFAULT_TIMEOUT";
-  private static final String SERVER_EVENT_ID_KEY = "SERVER_EVENT";
+    private static final String DEFAULT_PORT_KEY = "DEFAULT_PORT";
+    private static final String DEFAULT_TIMEOUT_KEY = "DEFAULT_TIMEOUT";
+    private static final String SERVER_EVENT_ID_KEY = "SERVER_EVENT";
 
-  private static final int DEFAULT_PORT = 8080;
-  private static final int DEFAULT_TIMEOUT = 30000;
-  private static final String SERVER_EVENT_ID = "reactNativeHttpServerResponse";
+    private static final int DEFAULT_PORT = 8080;
+    private static final int DEFAULT_TIMEOUT = 30000;
+    private static final String SERVER_EVENT_ID = "reactNativeHttpServerResponse";
 
-  private int _port;
-  private int _timeout;
+    private static final String ERROR_SERVER_NOT_RUNNING = "ERROR_SERVER_NOT_RUNNING";
+    private static final String ERROR_COULD_NOT_START = "ERROR_COULD_NOT_START";
 
-  private Server _server = null;
+    private int port;
+    private int timeout;
+    private Server server = null;
 
-  private Callback _success;
-  private Callback _failure;
 
-  public RNHttpServerModule(ReactApplicationContext reactContext) {
-    super(reactContext);
+    public RNHttpServerModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
 
-    _port = DEFAULT_PORT;
-    _timeout = DEFAULT_TIMEOUT;
-
-    reactContext.addLifecycleEventListener(this);
-
-    this.reactContext = reactContext;
-
-  }
-
-  @Override
-  public String getName() {
-    return "RNHttpServer";
-  }
-
-  @Override
-  public Map<String, Object> getConstants() {
-    final Map<String, Object> constants = new HashMap<>();
-    constants.put(DEFAULT_PORT_KEY, DEFAULT_PORT);
-    constants.put(DEFAULT_TIMEOUT_KEY, DEFAULT_TIMEOUT);
-    constants.put(SERVER_EVENT_ID_KEY, SERVER_EVENT_ID);
-    return constants;
-  }
-
-  @ReactMethod
-  public void init(ReadableMap options, Callback success, @Nullable Callback failure) {
-
-    Log.d(TAG, "Initialising server...");
-
-    if(options.hasKey("port")) {
-      _port = options.getInt("port");
+        port = DEFAULT_PORT;
+        timeout = DEFAULT_TIMEOUT;
     }
 
-    if(options.hasKey("timeout")) {
-      _timeout = options.getInt("timeout");
+    @Override
+    public String getName() {
+        return "RNHttpServer";
     }
 
-    start(success, failure);
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put(DEFAULT_PORT_KEY, DEFAULT_PORT);
+        constants.put(DEFAULT_TIMEOUT_KEY, DEFAULT_TIMEOUT);
+        constants.put(SERVER_EVENT_ID_KEY, SERVER_EVENT_ID);
 
-  }
+        return constants;
+    }
 
-  @ReactMethod
-  public void start(@Nullable Callback success, @Nullable Callback failure) {
+    @ReactMethod
+    public void init(ReadableMap options) {
+        Log.d(TAG, "Initializing server...");
 
-      try {
-
-        _server = new Server(reactContext, _port, _timeout);
-        _server.start();
-
-        if(success != null) {
-          success.invoke();
+        if (options.hasKey("port")) {
+            port = options.getInt("port");
         }
 
-      }
-      catch(Exception e) {
-        Log.e(TAG, e.getMessage());
-
-        if(failure != null) {
-          failure.invoke(e.getMessage());
+        if (options.hasKey("timeout")) {
+            timeout = options.getInt("timeout");
         }
-
-      }
-
-  }
-
-  @ReactMethod
-  public void stop() {
-    Log.d(TAG, "Server Stopped.");
-    _server.stop();
-    _server = null;
-  }
-
-  @ReactMethod
-  public void setResponse(String uri, ReadableMap response) {
-    if(_server != null) {
-      _server.setResponse(uri, response);
     }
-  }
 
-  @ReactMethod
-  public String getHostName() {
-    if(_server != null) {
-      Log.d(TAG, _server.getHostname());
-      return _server.getHostname();
+    @ReactMethod
+    public void start(Promise promise) {
+        try {
+            server = new Server(reactContext, port, timeout);
+            server.start();
+            port = server.getListeningPort();
+
+            WritableMap args = Arguments.createMap();
+            args.putString("hostname", server.getHostname());
+            args.putString("port", String.valueOf(server.getListeningPort()));
+
+            promise.resolve(args);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+            promise.reject(ERROR_COULD_NOT_START, e);
+        }
     }
-    else {
-      return "not defined";
+
+    @ReactMethod
+    public void stop() {
+        if (server == null) { return; }
+
+        server.stop();
+        server = null;
+        Log.d(TAG, "Server stopped.");
     }
-  }
 
-  /* Shut down the server if app is destroyed or paused */
-  @Override
-  public void onHostResume() {
-    //we can restart the server here as the success callback is not needed since an event is registered
-    start(null, null);
-  }
-
-  @Override
-  public void onHostPause() {
-    stop();
-  }
-
-  @Override
-  public void onHostDestroy() {
-    stop();
-  }
-
+    @ReactMethod
+    public void setResponse(String uri, ReadableMap response) {
+        if (server == null) { return; }
+        server.setResponse(uri, response);
+    }
 }
